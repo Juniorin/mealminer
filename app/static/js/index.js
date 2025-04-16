d53e68c6a1884d57f2737ab092775fc42a4abb5b
John Le <jjuniorin@gmail.com>
2025-04-16 01:33:21 -0700

    starting on rain js

---
+++ app/static/js/index.js
@@ -0,0 +1,9 @@
+const images = [
+    "{{ url_for('static', filename='images/index_images/butter.png') }}",
+    "{{ url_for('static', filename='images/index_images/egg.png') }}",
+    "{{ url_for('static', filename='images/index_images/garlic.png') }}",
+    "{{ url_for('static', filename='images/index_images/greenonion.png') }}",
+    "{{ url_for('static', filename='images/index_images/redonion.png') }}",
+    "{{ url_for('static', filename='images/index_images/tomato.png') }}",
+    "{{ url_for('static', filename='images/index_images/water.png') }}"
+];
